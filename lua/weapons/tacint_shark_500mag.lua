ca1de6d763cce3bce4c1e6148c608d5f85f51c91
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-03-01 12:53:58 -0500

    500 magnum lua

--- lua/weapons/tacint_shark_500mag.lua
+++ lua/weapons/tacint_shark_500mag.lua
@@ -1,13 +1,13 @@
 SWEP.Base = "tacint_base"
-SWEP.Spawnable = false
+SWEP.Spawnable = true
 
 AddCSLuaFile()
 
 // names and stuff
-SWEP.PrintName = "Raging Bull"
+SWEP.PrintName = ".500 Magnum"
 SWEP.Category = "RMC Interops (Handguns)"
 
-SWEP.Description = "Powerful revolver with excellent accuracy but poor damage retention."
+SWEP.Description = "Massive revolver that's lethal up close but has incredible recoil."
 
 SWEP.ViewModel = "models/weapons/tacint/v_mr96.mdl"
 SWEP.WorldModel = "models/weapons/tacint/w_mr96.mdl"
@@ -16,15 +16,15 @@ SWEP.Slot = 1
 
 // "ballistics"
 
-SWEP.Damage_Max = 100 / 2 // damage at minimum range
-SWEP.Damage_Min = 100 / 3 // damage at maximum range
-SWEP.Range_Min = 800 // distance for which to maintain maximum damage
-SWEP.Range_Max = 2375 // distance at which we drop to minimum damage
-SWEP.Penetration = 1.5 // units of metal this weapon can penetrate
+SWEP.Damage_Max = 67 // damage at minimum range
+SWEP.Damage_Min = 100 / 4 // damage at maximum range
+SWEP.Range_Min = 400 // distance for which to maintain maximum damage
+SWEP.Range_Max = 900 // distance at which we drop to minimum damage
+SWEP.Penetration = 3 // units of metal this weapon can penetrate
 
 SWEP.BodyDamageMultipliers = {
     [HITGROUP_HEAD] = 2,
-    [HITGROUP_CHEST] = 1,
+    [HITGROUP_CHEST] = 1.5,
     [HITGROUP_STOMACH] = 1,
     [HITGROUP_LEFTARM] = 1,
     [HITGROUP_RIGHTARM] = 1,
@@ -37,42 +37,42 @@ SWEP.BodyDamageMultipliers = {
 
 SWEP.Firemode = 1
 
-SWEP.RPM = 400
+SWEP.RPM = 350
 
 SWEP.Spread = 0.001
 
 // Spread penalties are in spread units and are additive
 SWEP.MoveSpreadPenalty = 0.03 // spread penalty while travelling at max. 250 u/s
 SWEP.MidAirSpreadPenalty = 0.065 // spread penalty for being in the air
-SWEP.HipFireSpreadPenalty = 0 // spread penalty for not being scoped in
-SWEP.ScopedSpreadPenalty = -0.0005
+SWEP.HipFireSpreadPenalty = 0.002 // spread penalty for not being scoped in
+SWEP.ScopedSpreadPenalty = 0.005
 SWEP.BlindFireSpreadPenalty = 0 // spread penalty for blind firing
 SWEP.CrouchSpreadPenalty = -0.006
 
-SWEP.RecoilPerShot = 1
-SWEP.RecoilMaximum = 4
+SWEP.RecoilPerShot = 1.5
+SWEP.RecoilMaximum = 10
 SWEP.RecoilResetTime = 0.2 // time after you stop shooting for recoil to start dissipating
 SWEP.RecoilDissipationRate = 5
 SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
 
-SWEP.RecoilVisualKick = 0.05
+SWEP.RecoilVisualKick = 3
 SWEP.RecoilVisualShake = 0.85
 
-SWEP.RecoilKick = 1.5
+SWEP.RecoilKick = 3 -- this gun is actually high recoil, ARCTIC!!!!!!!!!!!
 
-SWEP.RecoilSpreadPenalty = 0.065 // extra spread per one unit of recoil
+SWEP.RecoilSpreadPenalty = 0.075 // extra spread per one unit of recoil
 
 SWEP.CanBlindFire = true
 
 // handling
 
-SWEP.MoveSpeedMult = 0.98
+SWEP.MoveSpeedMult = 0.95
 SWEP.ShootingSpeedMult = 1 // slow down applied while shooting
 SWEP.SightedSpeedMult = 0.5
 SWEP.MeleeSpeedMult = 1
 SWEP.MeleeSpeedMultTime = 2 // seconds to apply slow down for
 
-SWEP.SprintToFireTime = 0.25
+SWEP.SprintToFireTime = 0.35
 
 // hold types
 
@@ -94,10 +94,10 @@ SWEP.SprintPos = Vector(2, 0, -12)
 
 // reload
 
-SWEP.ClipSize = 6
+SWEP.ClipSize = 5
 SWEP.Ammo = "357"
 
-SWEP.ReloadTimeMult = 1
+SWEP.ReloadTimeMult = 1.2
 
 // sounds
 
