c18587e697a088613765fc4a6250b9a905d30b96
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-03-01 10:40:13 -0500

    colt smg

---
+++ lua/weapons/tacint_shark_coltsmg.lua
@@ -0,0 +1,225 @@
+SWEP.Base = "tacint_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "Colt SMG"
+SWEP.Category = "RMC Interops (Small Arms)"
+
+SWEP.Description = "Burst-fire 9mm SMG derived from the M4 Carbine.  Offers excellent recoil control but sluggish handling."
+
+SWEP.ViewModel = "models/weapons/tacint_shark/v_coltsmg.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_m4.mdl"
+
+SWEP.Slot = 2
+
+// "ballistics"
+
+SWEP.Damage_Max = 100 / 5 // damage at minimum range
+SWEP.Damage_Min = 100 / 8 // damage at maximum range
+SWEP.Range_Min = 725 // distance for which to maintain maximum damage
+SWEP.Range_Max = 1500 // distance at which we drop to minimum damage
+SWEP.Penetration = 2 // units of metal this weapon can penetrate
+
+// misc. shooting
+
+SWEP.Firemode = -3
+
+SWEP.RPM = 900
+
+SWEP.Spread = 0
+
+// Spread penalties are in spread units and are additive
+SWEP.MoveSpreadPenalty = 0.045 // spread penalty while travelling at max. 250 u/s
+SWEP.MidAirSpreadPenalty = 0.1 // spread penalty for being in the air
+SWEP.HipFireSpreadPenalty = 0.025 // spread penalty for not being scoped in
+SWEP.ScopedSpreadPenalty = 0.01
+SWEP.BlindFireSpreadPenalty = 0.05 // spread penalty for blind firing
+SWEP.CrouchSpreadPenalty = -0.002
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 10
+SWEP.RecoilResetTime = 0.25 // time after you stop shooting for recoil to start dissipating
+SWEP.RecoilDissipationRate = 100
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 0.2
+SWEP.RecoilVisualShake = 0.25
+
+SWEP.RecoilKick = 0.65
+
+SWEP.RecoilSpreadPenalty = 0.002 // extra spread per one unit of recoil
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.96
+SWEP.ShootingSpeedMult = 1 // slow down applied while shooting
+SWEP.SightedSpeedMult = 0.5
+SWEP.MeleeSpeedMult = 1
+SWEP.MeleeSpeedMultTime = 2 // seconds to apply slow down for
+
+SWEP.SprintToFireTime = 0.4 // multiplies how long it takes to recover from sprinting
+
+// hold types
+
+SWEP.HoldType = "ar2"
+SWEP.HoldTypeSprint = "passive"
+SWEP.HoldTypeBlindFire = false
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -6)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(0, -2, -4)
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
+
+// reload
+
+SWEP.ClipSize = 32
+SWEP.Ammo = "pistol"
+
+SWEP.ReloadTimeMult = 1.03
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/m4.mdl"
+SWEP.DropMagazineImpact = "metal"
+
+// sounds
+
+local path = "tacint_shark/coltsmg/"
+local path1 = "tacint/weapons/m4/"
+local path2 = "tacint_extras/mac10/"
+
+SWEP.Sound_Shoot = "^" .. path .. "car-151.wav"
+SWEP.Sound_Shoot_Silenced = path2 .. "mac10-suppressed-1.wav"
+
+SWEP.Vol_Shoot = 110
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_ak47"
+SWEP.EjectEffect = 1
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+	["fire1"] = "fire1_M",
+    ["fire2"] = "fire2_M",
+    ["fire3"] = "fire3_M",
+    ["fire4"] = "fire4_M",
+    ["fire5"] = "fire5_M",
+    ["melee"] = {"melee1", "melee2"}
+}
+
+SWEP.DeployTimeMult = 1.25
+
+// attachments
+
+SWEP.AttachmentElements = {
+    ["foldstock"] = {
+        BGs_VM = {
+            {1, 1}
+        },
+    },
+    ["tactical"] = {
+        BGs_VM = {
+            {3, 1}
+        },
+    },
+}
+
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Optic",
+        Category = {"optic_smg", "optic_cqb", "optic_medium"},
+        InstalledElements = {"tactical"},
+        Bone = "ValveBiped.m4_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/optic_on.wav",
+        DetachSound = "tacint/weapons/optic_off.wav",
+        VMScale = 1,
+        Pos_VM = Vector(-7.4, 0, 5),
+        Pos_WM = Vector(0.75, 5, 1.15),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, -90 + 3.5, 0),
+    },
+    [2] = {
+        PrintName = "Muzzle",
+        Category = "silencer",
+        Bone = "ValveBiped.m4_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/silencer_on.wav",
+        DetachSound = "tacint/weapons/silencer_off.wav",
+		VMScale = 0.9,
+        Pos_VM = Vector(-4.1, 0.1, 22),
+        Pos_WM = Vector(1.4, 21, -0.5),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, -90 + 3.5, 0),
+    },
+    [3] = {
+        PrintName = "Tactical",
+        Category = "tactical",
+        Bone = "ValveBiped.m4_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+        Pos_VM = Vector(-5.1, -0.8, 13.5),
+        Pos_WM = Vector(2, 13, -0.5),
+        Ang_VM = Angle(90, 0, -45),
+        Ang_WM = Angle(-90, -90 + 3.5, 0),
+    },
+    [4] = {
+        PrintName = "Perk",
+        Category = {"foldstock", "perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_coltsmg.Remove_Clip", path1 .. "remove_clip.wav")
+addsound("tacint_coltsmg.Insert_Clip", path .. "magin.wav")
+addsound("tacint_coltsmg.Insert_Clip-mid", path .. "magin.wav")
+addsound("tacint_coltsmg.bolt_action", path1 .. "m4_bolt_action.wav")
+addsound("tacint_coltsmg.bolt_slap", path .. "boltcatch.wav")
+addsound("tacint_coltsmg.throw_catch", path1 .. "m4_throw_catch.wav")
